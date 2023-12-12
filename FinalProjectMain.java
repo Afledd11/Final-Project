c03084b50ccffa2936ce3a55b9f34cdc5e0c40d0
ChrisB2323 <150302970+ChrisB2323@users.noreply.github.com>
2023-12-12 13:43:07 -0500

    Update FinalProjectMain.java
    Final Copy

--- FinalProjectMain.java
+++ FinalProjectMain.java
@@ -7,20 +7,17 @@ import javafx.scene.Scene;
 
 public class FinalProjectMain extends Application
 {
-
    public static void main(String[] args)
    {
       launch(args);
    }
-   
    @Override
    public void start(Stage stage) throws Exception
    {
     // Load the FXML file
         Parent root = FXMLLoader.load(getClass().getResource("UI.fxml"));
     // Set up the scene    
-        Scene scene = new Scene(root); 
-        
+        Scene scene = new Scene(root);    
         stage.setTitle("NBA Stat Tracker"); 
         stage.setScene(scene);
         stage.show(); 
